bac02674fa71f068d5b9055dbdba75728c5feb25
sealteam21 <dfarley@uncc.edu>
2021-09-04 18:18:51 -0700

    solution of assignment

--- activity-hello/hello.cpp
+++ activity-hello/hello.cpp
@@ -1,4 +1,16 @@
-int main () {
+#include <unistd.h>
+#include <iostream>
 
+using namespace std;
+
+int main() {
+
+  int variable = 128;
+  char array[128];
+ auto result = gethostname(array, variable);
+  cout << string(array);
   return 0;
 }
+
+
+
